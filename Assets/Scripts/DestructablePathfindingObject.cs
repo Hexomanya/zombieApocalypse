cd2287d0f239518c1177767b3a656a5219b1e4dc
Benjamin Schön <Hexomanya@gmail.com>
2021-11-02 20:03:59 +0100

    Testing Universal 2D Rendering for lighting

---
+++ Assets/Scripts/DestructablePathfindingObject.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructablePathfindingObject : MonoBehaviour
+{
+
+    [SerializeField] private bool testDestroyBool = false;
+
+    Bounds bounds;
+
+    private void Start()
+    {
+        bounds = this.GetComponent<Collider2D>().bounds;
+    }
+
+    private void Update()
+    {
+        if (testDestroyBool)
+        {
+            Destroy(this.gameObject);
+        }    
+    }
+
+    private void OnDestroy()
+    {
+        if(bounds != null && AstarPath.active != null)
+        {
+            AstarPath.active.UpdateGraphs(bounds);
+        }
+    }
+}
