e47855ec3af07f9718a02be477146378507dd774
LUKEx76 <ruf.luca@web.de>
2021-11-04 17:58:32 +0100

    BodyPart Prefabs created, Drag and Drop implemented, Scrollbehavior implemented

---
+++ Assets/Scripts/BodyParts/BodyPartType.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BodyPartType 
+{
+    Head,
+    LeftArm,
+    RightArm,
+    LeftFoot,
+    RightFoot
+}
---
+++ Assets/Scripts/BodyParts/LeftArm.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftArm : IBodyPartType
+{
+    public int strength => 10;
+}
