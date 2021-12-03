c30d6825176e7c5d0d6718d386c579a073da55d3
LUKEx76 <ruf.luca@web.de>
2021-12-03 15:48:54 +0100

    InGame UI and LevelEnd PopUps

---
+++ Assets/Scripts/UI/BodyPartInventorySlot.cs
@@ -0,0 +1,73 @@
+using TMPro;
+using UnityEngine;
+
+public class BodyPartInventorySlot : MonoBehaviour
+{
+    public BodyPartType bodyPartType;
+
+    public ZombieType zombieType;
+
+    public float displayTime = 2f;
+
+    public int offSet = 20;
+
+    public bool onlyNewParts = false;
+
+    private float _currentDisplayTime;
+
+    private GameObject _popUp;
+
+    private Inventory _inventory;
+
+    private TMP_Text _textMesh;
+
+    private void Awake()
+    {
+        _inventory = Inventory.instance;
+        _textMesh = GetComponentInChildren<TMP_Text>();
+    }
+
+    private void Start()
+    {
+        if (onlyNewParts)
+            _textMesh.text = _inventory.GetAmountOfNewBodyPart(bodyPartType, zombieType).ToString();
+        else
+            _textMesh.text = _inventory.GetTotalAmountOfBodyPart(bodyPartType, zombieType).ToString();
+    }
+
+    void Update()
+    {
+        if(_currentDisplayTime > 0f)
+        {
+            _currentDisplayTime -= Time.deltaTime;
+        }
+        else if (_popUp != null)
+        {   
+            Destroy(_popUp);
+        }
+    }
+
+    public void UpdateUI()
+    {
+        if(onlyNewParts)
+        {
+            _textMesh.text = _inventory.GetAmountOfNewBodyPart(bodyPartType, zombieType).ToString();
+        }
+        else
+        {
+            _textMesh.text = _inventory.GetTotalAmountOfBodyPart(bodyPartType, zombieType).ToString();
+            ShowBodyPartCollectedPopUp();
+        }
+    }
+
+    public void ShowBodyPartCollectedPopUp()
+    {
+        _currentDisplayTime = displayTime;
+        if (_popUp != null)
+            return;
+        _popUp = Instantiate(gameObject, transform);
+        Destroy(_popUp.GetComponent<BodyPartInventorySlot>());
+        _popUp.transform.position = new Vector3(_popUp.transform.position.x + 40, _popUp.transform.position.y - offSet, 0);
+        _popUp.GetComponentInChildren<TMP_Text>().text = "+";
+    }
+}
