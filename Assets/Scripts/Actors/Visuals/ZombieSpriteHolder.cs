56e0d973c5153d0eb4a7e1ae6865a35372b85470
AJannik <jannik.schmidberger@gmail.com>
2021-12-08 12:23:13 +0100

    Added Sprite handling

---
+++ Assets/Scripts/Actors/Visuals/ZombieSpriteHolder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Actors.Visuals
+{
+    [Serializable]
+    public class ZombieSpriteHolder
+    {
+        [field: SerializeField] public ZombieType ZombieType { get; private set; }
+        [field: SerializeField] public BodyPartType BodyPartType { get; private set; }
+        [field: SerializeField] public SpriteOrientation SpriteOrientation { get; private set; }
+
+        [field: SerializeField] public Sprite ZombieSprite { get; private set; }
+    }
+}
