c81ac86c2040acab912fcffa95b760725dac4672
Benjamin Schön <Hexomanya@gmail.com>
2021-12-20 15:01:04 +0100

    Fixed Bullets and minor changes.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -3,10 +3,21 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    [SerializeField] private LayerMask zombieLayer;
+
     private float ttl = 5f;
     public float Damage { get; set; } = 0f;
     public Vector3 Direction { get; set; }
     public float Velocity { get; set; }
+
+
+    private void Start()
+    {
+        Quaternion rotation = Quaternion.LookRotation(Direction.normalized);
+        rotation.x = this.transform.rotation.x;
+        rotation.y = this.transform.rotation.y;
+        this.transform.rotation = rotation;
+    }
 
     void Update()
     {
@@ -21,12 +32,19 @@ public class Projectile : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        AttackableObject attackableObject = collision.gameObject.GetComponent<AttackableObject>();
-        if (attackableObject != null)
+        //Because it collides with Visuals now
+        GameObject actualZombie = collision.transform.parent.gameObject;
+
+        if(actualZombie != null)
         {
-            Vector3 dir = (attackableObject.transform.position - transform.position).normalized;
-            Quaternion rotation = Quaternion.FromToRotation(Vector3.right, dir);
-            attackableObject.ApplyDamage(Damage, rotation);
+            AttackableObject attackableObject = actualZombie.GetComponent<AttackableObject>();
+            if (attackableObject != null)
+            {
+                Debug.Log("Did " + Damage + " Point of damage Damage with bullet");
+                Vector3 dir = (attackableObject.transform.position - transform.position).normalized;
+                Quaternion rotation = Quaternion.FromToRotation(Vector3.right, dir);
+                attackableObject.ApplyDamage(Damage, rotation);
+            }
         }
 
         Destroy(gameObject);
